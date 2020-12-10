acf7ef63f6d383fe3b370e0a004684bf5664824b
foeu <fabiano.junior@gft.com>
2020-12-10 09:43:58 -0300

    Validação das credenciais de login

--- DTO/CredenciaisDTO.cs
+++ DTO/CredenciaisDTO.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projeto.DTO
 {
     public class CredenciaisDTO
     {
+        [Required(ErrorMessage = "Necessário informar e-mail para login")]
+        [EmailAddress(ErrorMessage = "E-mail está em formato inválido")]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Necessário informar senha para login")]
+        [StringLength(12, ErrorMessage = "A senha deve ter de 6 até 12 caracteres", MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
     }
 }
